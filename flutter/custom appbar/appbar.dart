d1a0cf957f3bfd45fee51a37883d2d24ab18965b
jopaul23 <jopauljoy999@gmail.com>
2022-10-22 12:36:20 +0530

    unused packages removed

--- flutter/custom appbar/appbar.dart
+++ flutter/custom appbar/appbar.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:m3homes/view/constants/constants.dart';
 
 class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppbar({
