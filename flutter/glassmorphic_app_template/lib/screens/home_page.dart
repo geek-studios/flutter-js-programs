11f6072cb3e48e6b0549bc4fca55eed505f592dd
Sreejith S Kartha <sreejithskartha@gmail.com>
2022-10-31 21:49:12 +0530

    added the bottom model sheet

--- flutter/glassmorphic_app_template/lib/screens/home_page.dart
+++ flutter/glassmorphic_app_template/lib/screens/home_page.dart
@@ -7,6 +7,7 @@ import 'package:glassmorphic_app_template/widgets/glassy_navbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
   final String image =
       "https://freepngimg.com/download/water_drops/29328-6-water-drops-transparent-background.png";
   @override
@@ -30,11 +31,69 @@ class HomePage extends StatelessWidget {
                 'assets/images/44b208e04a532e7077071d32baebf666.jpeg'),
           ),
         ),
-        child: const Center(
+        child: Center(
           child: GlassyContainer(
-              text: "Focus on the \nJOURNEY,\nnot the destination!"),
+            text: "Focus on the \nJOURNEY,\nnot the destination!",
+            onPressed: () {
+              showSheet(context);
+            },
+          ),
         ),
       ),
     );
   }
+}
+
+showSheet(BuildContext context) {
+  showModalBottomSheet<void>(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      context: context,
+      barrierColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+                border: Border(top: BorderSide(color: Colors.white, width: 2)),
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15)),
+                gradient: const LinearGradient(
+                    colors: [Colors.white60, Colors.white10],
+                    end: Alignment.topCenter,
+                    begin: Alignment.bottomRight),
+                image: const DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                      "assets/images/noisy-background_1194-7547 (1).jpg"),
+                  opacity: 0.1,
+                  // colorFilter:
+                  //     ColorFilter.mode(Colors.white10, BlendMode.dstATop),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.white.withOpacity(0.1),
+                      blurRadius: 25,
+                      spreadRadius: 0)
+                ],
+              ),
+              child: SizedBox(
+                height: 250,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "Bottom Model Sheet",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      });
 }
